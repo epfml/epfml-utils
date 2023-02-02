3afb0ea35073bdf529d49ad944e8adc2cf15f712
Thijs Vogels <thijs.vogels@epfl.ch>
2023-02-02 14:16:47 +0100

    setup

--- src/mlotools/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-keyval_access_key = os.getenv("MLO_KEYVAL_S3_ACCESS_KEY", None)
-keyval_secret_key = os.getenv("MLO_KEYVAL_S3_SECRET_KEY", None)
-keyval_endpoint = os.getenv("MLO_KEYVAL_S3_ENDPOINT", None)
-keyval_bucket = os.getenv("MLO_KEYVAL_S3_BUCKET", None)
-default_user = os.getenv("MLO_LDAP", os.getenv("USER", None))
